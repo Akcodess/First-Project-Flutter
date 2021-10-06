fceb1fe27b35387feaa6899fecc2b1ed4da71f56
Akcodess <85602016+Akcodess@users.noreply.github.com>
2021-10-06 10:39:57 +0530

    Update welcome_screen.dart

--- lib/Screens/welcome/welcome_screen.dart
+++ lib/Screens/welcome/welcome_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'components/body.dart';
+import 'components/welcomebody.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body(),
+      body: WelcomeBody(),
     );
   }
 }
