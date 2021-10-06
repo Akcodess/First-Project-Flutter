d7c2df7d5a543f4d393d05253532d8c23b294758
Akcodess <85602016+Akcodess@users.noreply.github.com>
2021-10-06 10:43:00 +0530

    Add files via upload

---
+++ lib/Screens/welcome/welcome_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'components/welcomebody.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WelcomeBody(),
+    );
+  }
+}
